753ba83cdc1ee4768fd2647aa070e0259acde677
ambmt <ambmtgg@gmail.com>
2024-02-11 17:53:12 +0000

    Simulation finally works. V0.01

--- core/src/com/ambmt/simulator/simulation/MainSim.java
+++ core/src/com/ambmt/simulator/simulation/MainSim.java
@@ -3,6 +3,7 @@ package com.ambmt.simulator.simulation;
 import com.ambmt.simulator.players.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -12,7 +13,7 @@ public class MainSim {
     private boolean gameActive = true;
     private List<Boolean> templateList = new ArrayList<>();
     private TempSim tempSim;
-    private int atBatCount = 1;
+    private boolean[] runners;
     private Random random;
     private int outs = 0;
     private int innings = 0;
@@ -21,52 +22,41 @@ public class MainSim {
     private int homeIndex;
     private int awayIndex;
     private String edgePos;
+    private int homeScore;
+    private int awayScore;
 
     public void MainSim(){
         random = new Random();
-        templateList.add(0, false);
-        templateList.add(1, false);
-        templateList.add(2, false);
-        templateList.add(3, false);
-        templateList.add(4, false);
         tempSim = new TempSim();
         this.outs = 0;
         this.innings = 0;
 
     }
-    public List<Boolean> newInning(){
-        List<Boolean> runners = new ArrayList<>();
-        runners.add(0,true); // Home - therefore this has to be true as a batter always has to be at the plate
-        runners.add(1,false); // First
-        runners.add(2,false); // Second
-        runners.add(3, false); // Thirdv
-        runners.add(4, false); // A runner has scored
-        return runners;
-    }
-
     public void startGame(){
-        List<Boolean> runners= newInning();
         TeamBuilder tb = new TeamBuilder();
         tb.importJSON(0,0);
         homeTeam = tb.homeTeam;
         awayTeam = tb.awayTeam;
+        runners = new boolean[4];
         boolean homeActive = false;
         Team activeTeam;
         while(gameActive){
             int result;
             int strikes = 0;
-            newAb(runners);
-            int newIndex;
+            playerOnBase();
+            int newIndex = 0;
             Pitcher pitcher;
             Batter batter;
+
+            int pitchCount;
             if(homeActive){
                 pitcher = (Pitcher) awayTeam.getPitchers().get(0);
-                newIndex = homeIndex;
+                newIndex += homeIndex;
                 batter = (Batter) homeTeam.getBatters().get(newIndex);
             }
             else{
                 pitcher = (Pitcher) homeTeam.getPitchers().get(0);
-                newIndex = awayIndex;
+                newIndex += awayIndex;
                 batter = (Batter) awayTeam.getBatters().get(newIndex);
             }
             int margin = calculateMargin(batter, pitcher);
@@ -78,28 +68,45 @@ public class MainSim {
                 edgePos = "pitcher";
                 margin *= -1;
             }
+            pitchCount = 1;
             do{
-                result = tempSim.calculatePitchOutcome(1,false,edgePos,margin,0,random);
+                result = tempSim.calculatePitchOutcome(pitchCount,false,edgePos,margin,0,random);
+                pitchCount++;
                 if(result == 1){
                     strikes++;
                 }
             }while (result !=3 && strikes <= 2);
+            newIndex++;
             if(result == 3){
                     if(random.nextInt(1000)+ 1 <= 248 ){ // This is the average in the mlb. The margin is already used in the simulation code, therefore I decided to use this
-                        runners = generateRandomHit(runners, batter,pitcher);
+                        generateRandomHit(batter,pitcher);
                     }
                     else{
                         outs++;
-                        Out(runners, batter, outs);
+                        Out(batter, outs);
                     }
             }
             if (strikes == 3){
                 outs++;
-                strikeOut(batter,pitcher);
+                strikeOut(batter,pitcher, outs);
             }
             if (outs >= 3){
-                runners = newInning();
-                innings++;
+                System.out.println("Home: " +homeScore);
+                System.out.println("Away: " + awayScore);
+                clearBases();
+                if(homeActive){
+                    homeScore += runs;
+                    runs = 0;
+                }
+                else{
+                    awayScore += runs;
+                    runs = 0;
+                }
+                System.out.println(Arrays.toString(runners));
+                // A new innning should only be called when the home team has just batted. This being called all the time shortened the game significantly
+                if(homeActive){
+                    innings++;
+                }
                 outs = 0;
                 if(newIndex == 7){
                     // Resetting the lineup
@@ -110,15 +117,16 @@ public class MainSim {
                         awayIndex = newIndex;
                     }
                 }
+
                 homeActive = !homeActive;
 
             }
             // Temp sim
-            if (innings > 3){
+            if (innings > 8){
                 gameActive = false;
+                System.out.println(homeScore + " score for the home team");
+                System.out.println(awayScore + " score for the away team");
             }
-
-
 
         }
     }
@@ -128,95 +136,34 @@ public class MainSim {
     // 63-80 = Double
     // 80 - 95 = Homerun
     // 95 - 100 = Triple
-    private List<Boolean> generateRandomHit(List<Boolean> runners, Batter batter, Pitcher pitcher){
+    private void generateRandomHit(Batter batter, Pitcher pitcher){
         int hit = random.nextInt(100) + 1;
         if (hit <= 63){
-            runners = Single(runners);
+            hitSingle();
+            System.out.println(Arrays.toString(runners) + " single ");
             callUpdate(batter.getName() + " singles off " + pitcher.getName());
-            return runners;
         }
         if(hit <= 80){
-            runners = Double(runners);
+            hitDouble();
             callUpdate(batter.getName() + " doubles off " + pitcher.getName());
-            return runners;
         }
         if(hit <= 95){
-            Homerun(runners);
+            hitHomeRun();
+            System.out.println(Arrays.toString(runners) + " homerun");
             callUpdate(batter.getName() + " hits a homerun off " + pitcher.getName());
-            return runners;
         }
         else{
             callUpdate(batter.getName() + " triples off " + pitcher.getName());
-            runners = Triple(runners);
+            hitTriple();
+            System.out.println(Arrays.toString(runners) + " triple");
 
         }
-        return runners;
 
 
     }
-
-
-    private List<Boolean> Single(List<Boolean> runners){
-        List<Boolean> tempList = templateList;
-        for (int i = 0; i < runners.size() - 1; i++){
-            if(runners.get(i)) {
-                int temp = i + 1;
-                if (temp >= 4) {
-                    runs += 1;
-                }else{
-                    tempList.set(temp, true);
-                }
-            }
-
-        }
-        return tempList;
-
-    }
-    private List<Boolean> Double(List<Boolean> runners){
-        List<Boolean> tempList = templateList;
-        for (int i = 0; i< runners.size(); i++){
-            if(runners.get(i)) {
-                int temp = i + 2;
-                if (temp >= 4) {
-                    runs += 1;
-                }
-                else {
-                    tempList.set(temp, true);
-                }
-            }
-
-        }
-        return tempList;
-
-    }
-    private List<Boolean> Triple(List<Boolean> runners){
-        List<Boolean> tempList = templateList;
-        for (int i = 0; i< runners.size(); i++){
-            if(runners.get(i)) {
-                int temp = i + 3;
-                if (temp >= 4) {
-                    runs += 1;
-                }else {
-                    tempList.set(temp, true);
-                }
-                runners.set(i, false);
-
-            }
-
-        }
-        return tempList;
-
-    }
-    private void Homerun(List<Boolean> runners){
-        for(int i = 0; i< runners.size(); i++){
-            if(runners.get(i)){
-                runs += 1;
-                runners.set(i, false);
-            }
-        }
-    }
+    
     // Using https://library.fangraphs.com/pitching/batted-ball/, I can determine what the league average is and therefore what the outcome will be
-    private void Out(List<Boolean> runners, Batter player, int outs){
+    private void Out(Batter player, int outs){
         int index = random.nextInt(100 + 1);
         if (index <= 21){
             // Linedrive
@@ -231,17 +178,12 @@ public class MainSim {
             callUpdate(player.getName() + " flies, " + outs + " down.");
         }
     }
-    private void strikeOut(Batter batter, Pitcher pitcher){
-        callUpdate(batter.getName() + " strikes out against " + pitcher.getName());
+    private void strikeOut(Batter batter, Pitcher pitcher, int outs){
+        callUpdate(batter.getName() + " strikes out against " + pitcher.getName() + "," + outs + " outs.");
     }
 
     private void callUpdate(String event){
         System.out.println(event);
-    }
-
-    private List<Boolean> newAb(List<Boolean> runners){
-        runners.set(0, true);
-        return runners;
     }
 
     private int calculateMargin(Batter batter, Pitcher pitcher){
@@ -249,6 +191,59 @@ public class MainSim {
         return margin;
     }
     // If it returns negative, its a pitcher advantage, if positive,batter
+
+
+
+    public int getRuns() {
+        return runs;
+    }
+
+    public void hitSingle() {
+        moveRunners(1);
+    }
+
+    public void hitDouble() {
+        moveRunners(2);
+    }
+
+    public void hitTriple() {
+        moveRunners(3);
+    }
+
+    public void hitHomeRun() {
+        moveRunners(4);
+    }
+
+    private void moveRunners(int basesToAdvance) {
+        // Move runners
+        for (int i = runners.length - 1; i >= 0; i--) {
+            if (runners[i]) {
+                if (i + basesToAdvance >= runners.length) {
+                    // A runner crosses home plate and scores a run
+                    runs++;
+                } else {
+                    runners[i + basesToAdvance] = true;
+                }
+                runners[i] = false;
+            }
+        }
+    }
+
+    private void clearBases() {
+        for (int i = 0; i < runners.length; i++) {
+            runners[i] = false;
+        }
+    }
+
+    public void playerOnBase() {
+        // Batter (home plate)
+        runners[0] = true;
+    }
+
+    public void printBaseStatus() {
+        System.out.println("Bases: " + runners[0] + " " + runners[1] + " " + runners[2] + " " + runners[3]);
+        System.out.println("Runs: " + runs);
+    }
 
 }
 
